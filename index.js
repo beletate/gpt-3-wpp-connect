4c4f592f8cfa47b1a5bbd7522da67234d3275fa8
Vinicius Beletate <vinicius_beletate@hotmail.com>
2023-04-25 19:17:03 -0300

    feat(code): function to respond people on whatsapp using gpt answers

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
+require('dotenv').config();
 const wppconnect = require('@wppconnect-team/wppconnect');
 
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const handleGptResponse = async (message) => {
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: 'user',
+        content: message
+      }
+    ],
+    temperature: 0.3
+  });
+  return response.data.choices[0].message.content;
+}
 wppconnect
   .create()
   .then((client) => start(client))
   .catch((error) => console.log(error));
 
 function start(client) {
-  client.onMessage((message) => {
-    if (message.body === 'Hello') {
+  client.onMessage(async (message) => {
+      const response = await handleGptResponse(message.body.slice(4))
       client
-        .sendText(message.from, 'Hello, how I may help you?')
+        .sendText(message.from, response)
         .then((result) => {
           console.log('Result: ', result); //return object success
         })
         .catch((erro) => {
           console.error('Error when sending: ', erro); //return object error
         });
-    }
   });
 }
